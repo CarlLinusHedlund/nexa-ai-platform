59d0f24e5ce0e903e3b21be48392518134dba293
Carl Linus Hedlund <89521533+CarlLinusHedlund@users.noreply.github.com>
2023-08-19 00:57:51 +0200

    updates layout file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 // import Image from 'next/image'
-
+'use-client'
 const item = {
   initial: { opacity: 0, y: 20 },
   animate: { opacity: 1, y: 0, transition: { duration: 1, ease: 'easeInOut', staggerChildren: 0.6 } },
